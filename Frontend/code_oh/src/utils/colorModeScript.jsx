f2eb1d9ba93e9a8fb365107960d432c753a39703
Felix <felixchen713@gmail.com>
2025-02-04 17:39:33 -0500

    Keep only database.sql in Felix branch

--- Frontend/code_oh/src/utils/colorModeScript.jsx
+++
@@ -1,6 +0,0 @@
-import { ColorModeScript as ChakraColorModeScript } from '@chakra-ui/react'
-import theme from './theme'
-
-export const ColorModeScript = () => {
-  return <ChakraColorModeScript initialColorMode={theme.config.initialColorMode} />
-}
